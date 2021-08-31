bb03425debc06dbc09088a589d7dd072ad45f712
Afonso Abreu <afonsohenrique2@hotmail.com>
2021-08-30 21:28:45 -0300

    Inclusão da área com serviços

--- src/app/shared/component/featurette/featurette.service.ts
+++ src/app/shared/component/featurette/featurette.service.ts
@@ -19,20 +19,13 @@ export class FeaturetteService{
         },
         {
             id: "servicos",
-            titulo: "Quem Somos", 
-            texto: "Somos uma empresa com foco no desenvolvimento de Landing Pages e Sites institucionais. Contamos com uma equipe de especialistas em Front-End e Back-end, no qual nos possibilita desenvolver o site ideal para a sua empresa e negócio!", 
-            pathImagem: "assets/img/quem-somos.jpg", 
+            titulo: "Nossos serviços", 
+            texto: "Desenvolvemos sites institucionais, lading pages, tudo utilizando as mais novas tecnologias e páginas totalmente responsivas, para que você possa oferecer a melhor experiência para seus clientes. Também oferecemos serviço de SEO para otimizar seu site nos buscadores e atrair cada vez mais clientes.", 
+            pathImagem: "assets/img/servicos.jpg", 
             ordemTexto: 2, 
             ordemImagem : 1
         },
-        {
-            id: "terceiro",
-            titulo: "Terceiro", 
-            texto: "Somos uma empresa com foco no desenvolvimento de Landing Pages e Sites institucionais. Contamos com uma equipe de especialistas em Front-End e Back-end, no qual nos possibilita desenvolver o site ideal para a sua empresa e negócio!", 
-            pathImagem: "assets/img/quem-somos.jpg", 
-            ordemTexto: 1, 
-            ordemImagem : 2
-        },
+        
     ] 
 
     retriveAll(): Featurette[]{
